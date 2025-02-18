ff04a01fbfd659e81afd5f06c77d42a46ed77111
spaghetti-lover <phungducanh2511@gmail.com>
2025-02-18 13:44:25 +0700

    (Update) Go.mod file

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ set -e
 
 echo "run db migration"
 source /app/app.env
-source app.env
 /app/migrate -path /app/migration -database "postgresql://root:Narutora12345@bank-system.cfcwm8aiq0n8.ap-southeast-2.rds.amazonaws.com:5432/postgres" -verbose up
 
 echo "start the app"
